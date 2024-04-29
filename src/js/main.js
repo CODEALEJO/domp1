6b449205652bf64e6fb8ea6d241dd74312f1469d
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-04-29 09:19:43 -0500

    termina la clase

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+let one = document.getElementById("btn-one")
+let two = document.getElementsByClassName("btn-two")
+let three = document.getElementsByTagName("btn-three")
+let four = document.getElementsByTagName("button")
+
+console.log(one)
+console.log(two[0])
+console.log(three)
+console.log(four)
+
+
+
+let success=document.querySelector("Success")
+let danger=document.querySelector("Success")
+let warning=document.querySelectorAll("warning")
+let info=document.querySelectorAll("info")
